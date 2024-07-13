aeebb85aab4c7b410dbcc668d71f059c273793a7
Daigo Shitara <shitara@tagbangers.co.jp>
2024-07-13 10:08:36 +0900

    simple ADT

---
+++ src/adt.ts
@@ -0,0 +1,59 @@
+import { match } from "ts-pattern";
+
+// with Union type
+// just a set of possible types
+type ResultWithUnion = string | number | (() => string);
+
+const resultProcessorWithUnion = (result: ResultWithUnion): string | undefined => {
+  if (typeof result === "number") {
+    return result.toFixed();
+  }
+
+  if (typeof result === "string") {
+    return result.toLocaleLowerCase();
+  }
+
+  return result();
+};
+
+// with Unit type
+// Unit type are a subtype of "primitive" types, that contain precisely one primitive value
+type ResultWithUnit = true | "error" | 5;
+
+const resultProcessorWithUnit = (result: ResultWithUnit): string => {
+  if (result === true) {
+    return "true";
+  }
+
+  if (result === "error") {
+    return result;
+  }
+
+  return "five";
+};
+
+resultProcessorWithUnit(3); // Error
+resultProcessorWithUnit(5); // Ok
+
+// Discriminated unions
+type Result =
+  | { type: "ok"; value: number } //
+  | { type: "error"; message: string };
+
+const resultInterpretor = (result: Result): string => {
+  switch (result.type) {
+    case "error":
+      return result.message;
+    case "ok":
+      return `${result.value}`;
+    default:
+      return "never" as never;
+  }
+};
+
+const betterResultInterpretor = (result: Result): string => {
+  return match(result)
+    .with({ type: "ok" }, res => `${res.value}`)
+    .with({ type: "error" }, err => `${err.message}`)
+    .exhaustive();
+};
